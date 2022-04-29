e4b6f1e47be14297801af1825e29d256f7f0ffec
mogannam <joemogannam@gmail.com>
2022-04-28 17:06:42 -0700

    add a category component to dynamically load categories from DB and add routes to home page

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,28 +1,21 @@
 import React, { createContext, useContext } from "react";
-import { useProductReducer } from './reducers';
-// useContext is a react hook allows us to use the state created by createContext
+import { useProductReducer } from './reducers'
 
 const StoreContext = createContext();
-//create context used to initiate a new context object. Create a contatiner to hold our
-// global state data and functionatilty to insert it into react
-
-
 const { Provider } = StoreContext;
 
 const StoreProvider = ({ value = [], ...props }) => {
-    const [state, dispatch] = useProductReducer({
-      products: [],
-      categories: [],
-      currentCategory: '',
-    });
-    // use this to confirm it works!
-    console.log(state);
-    return <Provider value={[state, dispatch]} {...props} />;
-  };
+  const [state, dispatch] = useProductReducer({
+    products: [],
+    categories: [],
+    currentCategory: '',
+  });
 
-  // used to insert the newly created component into the react app
-  const useStoreContext = () => {
-    return useContext(StoreContext);
-  };
+  return <Provider value={[state, dispatch]} {...props} />;
+};
 
-  export { StoreProvider, useStoreContext };
+const useStoreContext = () => {
+  return useContext(StoreContext);
+};
+
+export { StoreProvider, useStoreContext };
--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -1,8 +1,4 @@
 export const UPDATE_PRODUCTS = "UPDATE_PRODUCTS";
-// store data about products in this global state so we don't have to query all data
-// from the DB. Also, allows offline functionality.
-export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
-// similar to both above. If we update a product with new categoies
-// we need to update the category and products. This also allows data to persist offline too
-export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
 
+export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
+export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
